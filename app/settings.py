029d5d58b246c10347f8ed424672968a09386e0e
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-08-10 19:52:54 -0300

    nesse commit foi feito a base de criação para o front-end da aplicação.

--- app/settings.py
+++ app/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["app/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -110,6 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
