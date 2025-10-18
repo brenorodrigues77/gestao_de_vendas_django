3ff8f185b6de9bf4ed85000c764312b8a35b28eb
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-10-17 21:51:31 -0300

    implement product stock update on Inflow creation

--- inflow/apps.py
+++ inflow/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InflowConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "inflow"
+    
+    def ready(self):
+        import inflow.signals
