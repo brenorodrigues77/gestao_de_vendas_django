e4eba2348ff3c4e52f3eeb83539e43e24c4b0b1e
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-10-25 14:42:29 -0300

    inicializando o app de dashboard.

--- app/settings.py
+++ app/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "product",
     "inflow",
     "outflow",
+    "dashboard",
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("dashboard.urls")),
     path("", include("brands.urls")),
     path("", include("category.urls")),
     path("", include("supplier.urls")),
