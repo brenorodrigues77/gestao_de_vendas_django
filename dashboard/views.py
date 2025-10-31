3f903aefcc37c45f1fd85c47c7b08f8020040c72
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-10-30 22:07:45 -0300

    update product metrics display in dashboard template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 def dashboard_home(request):
-    context = {}
+    product_metrics = {
+        "total_products": 100,
+        "stock_cost": 500,
+        "total_profit": 700,
+        "stock_value": 200
+    }
+    
+    context = {
+        "product_metrics": product_metrics
+    }
     return render(request, "dashboard.html", context)
